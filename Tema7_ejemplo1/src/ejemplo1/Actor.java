bc087afc8836b4f893455ea16004da2f8c61b0fa
Ustavito <gustavo.marrero.tovar@gmail.com>
2021-04-08 18:58:31 +0200

    Añadida proyecto Tema7_ejemplo1

---
+++ Tema7_ejemplo1/src/ejemplo1/Actor.java
@@ -0,0 +1,31 @@
+package ejemplo1;
+
+public class Actor {
+	
+	private String nombre;
+	private String personaje;
+	
+	public Actor() {}
+	
+	public Actor (String n, String p) {
+		nombre = n;
+		personaje = p;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getPersonaje() {
+		return personaje;
+	}
+	
+	public void setNombre(String n) {
+		nombre = n;
+	}
+	
+	public void setPersonaje (String p) {
+		personaje = p;
+	}
+
+}
