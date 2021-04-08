cce65564e0fdd49b097f1c0c3bbbd250c2429130
Ustavito <gustavo.marrero.tovar@gmail.com>
2021-04-08 19:26:47 +0200

    Agregada clase pelicula

---
+++ Tema7_ejemplo1/src/ejemplo1/Pelicula.java
@@ -0,0 +1,32 @@
+package ejemplo1;
+
+import java.util.List;
+
+public class Pelicula {
+	private List<Actor> reparto;
+	private String titulo;
+	
+	public Pelicula() {}
+	
+	public Pelicula(String titulo, List<Actor> actores) {
+		this.titulo = titulo;
+		this.reparto = actores;
+	}
+	
+	public String getTitulo() {
+		return this.titulo;
+	}
+	
+	public List<Actor> getReparto(){
+		return this.reparto;
+	}
+	
+	public void setTitulo (String t) {
+		this.titulo = t;
+	}
+	
+	public void setReparto (List<Actor> actores) {
+		this.reparto = actores;
+	}
+
+}
